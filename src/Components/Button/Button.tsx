2f7a30548bdf7a5f5df960d3e90290d5e7b5aff9
Peter Kautwima <33910776+peter-kautwima@users.noreply.github.com>
2022-11-17 18:22:40 +0200

    add custom button component

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
+
+const Button = ({ label, onClick }: ButtonProps) => {
+  return (
+    <button className={styles.button} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
+
+export default Button;
