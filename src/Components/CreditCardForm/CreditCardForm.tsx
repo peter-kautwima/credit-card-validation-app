065ef7718bc4fdc54431ea04cfc7c1a557fcc6fe
Peter Kautwima <33910776+peter-kautwima@users.noreply.github.com>
2022-11-24 16:31:50 +0200

    update add card form layout

--- src/Components/CreditCardForm/CreditCardForm.tsx
+++ src/Components/CreditCardForm/CreditCardForm.tsx
@@ -8,6 +8,7 @@ import {
   validateRequired,
   validationNumberRange,
 } from "../../ValidationUtil/validation";
+import styles from "./CreditCardForm.module.scss";
 
 type Props = {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
@@ -135,62 +136,65 @@ const CreditCardForm = ({ onSubmit, bannedCountries, cards }: Props) => {
     handleValidaton();
   }, [values, touched]);
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        name="name"
-        label="Name"
-        value={values.name}
-        placeholder="John Doe"
-        onChange={(e) => setValues({ ...values, name: e.target.value })}
-        onBlur={() => setTouched({ ...touched, name: true })}
-        error={errors.name}
-        type="string"
-      />
-      <TextField
-        name="cardNumber"
-        label="Card Number"
-        value={values.cardNumber}
-        placeholder="XXXX XXXX XXXX XXXX"
-        onChange={(e) => setValues({ ...values, cardNumber: e.target.value })}
-        onBlur={() => setTouched({ ...touched, cardNumber: true })}
-        error={errors.cardNumber}
-        type="number"
-      />
-      <TextField
-        name="expirationDateMM"
-        label="Expiration Date: MM"
-        value={values.expirationDateMM}
-        placeholder="MM"
-        onChange={(e) =>
-          setValues({ ...values, expirationDateMM: e.target.value })
-        }
-        onBlur={() => setTouched({ ...touched, expirationDateMM: true })}
-        error={errors.expirationDateMM}
-        type="number"
-      />
-      <TextField
-        name="expirationDateYY"
-        label="Expiration Date: YY"
-        value={values.expirationDateYY}
-        placeholder="MM"
-        onChange={(e) =>
-          setValues({ ...values, expirationDateYY: e.target.value })
-        }
-        onBlur={() => setTouched({ ...touched, expirationDateYY: true })}
-        error={errors.expirationDateYY}
-        type="number"
-      />
-      <TextField
-        name="cvv"
-        label="CVV"
-        value={values.cvv}
-        placeholder="CVV"
-        onChange={(e) => setValues({ ...values, cvv: e.target.value })}
-        onBlur={() => setTouched({ ...touched, cvv: true })}
-        error={errors.cvv}
-        type="number"
-      />
-      <br />
+    <form onSubmit={handleSubmit} className={styles["credit-card-form"]}>
+      <div className={styles["two-col"]}>
+        <TextField
+          name="name"
+          label="Name"
+          value={values.name}
+          placeholder="John Doe"
+          onChange={(e) => setValues({ ...values, name: e.target.value })}
+          onBlur={() => setTouched({ ...touched, name: true })}
+          error={errors.name}
+          type="string"
+        />
+        <TextField
+          name="cardNumber"
+          label="Card Number"
+          value={values.cardNumber}
+          placeholder="XXXX XXXX XXXX XXXX"
+          onChange={(e) => setValues({ ...values, cardNumber: e.target.value })}
+          onBlur={() => setTouched({ ...touched, cardNumber: true })}
+          error={errors.cardNumber}
+          type="number"
+        />
+      </div>
+      <div className={styles["three-col"]}>
+        <TextField
+          name="expirationDateMM"
+          label="Expiration Date: MM"
+          value={values.expirationDateMM}
+          placeholder="MM"
+          onChange={(e) =>
+            setValues({ ...values, expirationDateMM: e.target.value })
+          }
+          onBlur={() => setTouched({ ...touched, expirationDateMM: true })}
+          error={errors.expirationDateMM}
+          type="number"
+        />
+        <TextField
+          name="expirationDateYY"
+          label="Expiration Date: YY"
+          value={values.expirationDateYY}
+          placeholder="YY"
+          onChange={(e) =>
+            setValues({ ...values, expirationDateYY: e.target.value })
+          }
+          onBlur={() => setTouched({ ...touched, expirationDateYY: true })}
+          error={errors.expirationDateYY}
+          type="number"
+        />
+        <TextField
+          name="cvv"
+          label="CVV"
+          value={values.cvv}
+          placeholder="CVV"
+          onChange={(e) => setValues({ ...values, cvv: e.target.value })}
+          onBlur={() => setTouched({ ...touched, cvv: true })}
+          error={errors.cvv}
+          type="number"
+        />
+      </div>
       <SelectCountry
         label="Select Country"
         name="country"
