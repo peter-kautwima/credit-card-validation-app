3b97ae5e527115b52b4420429623dee652d0b5b2
Peter Kautwima <33910776+peter-kautwima@users.noreply.github.com>
2022-11-28 13:03:16 +0200

    move form logic into custom useForm hook

--- src/Components/CreditCardForm/CreditCardForm.tsx
+++ src/Components/CreditCardForm/CreditCardForm.tsx
@@ -1,172 +1,20 @@
-import { useCallback, useEffect, useState } from 'react';
 import Button from '../Button/Button';
 import SelectCountry from '../SelectCountry/SelectCountry';
 import TextField from '../TextField/TextField';
 import { Card, Country } from '../../types';
-import {
-  validateLength,
-  validateRequired,
-  validationNumberRange,
-} from '../../ValidationUtil/validation';
 import styles from './CreditCardForm.module.scss';
+import useForm from './useForm';
 
-type Props = {
+export type Props = {
   onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
   cards: Card[];
   bannedCountries: Country[];
 };
 
-type Values = {
-  name: string;
-  cardNumber: string;
-  expirationDateMM: string;
-  expirationDateYY: string;
-  country: string;
-  cvv: string;
-};
-
-type Errors = {
-  [Property in keyof Values]?: string;
-};
-
-type Touched = {
-  [Property in keyof Values]?: boolean;
-};
-
-const initialValues = {
-  name: '',
-  cardNumber: '',
-  expirationDateMM: '',
-  expirationDateYY: '',
-  country: '',
-  cvv: '',
-};
-
 const CreditCardForm = ({ onSubmit, bannedCountries, cards }: Props) => {
-  const [values, setValues] = useState<Values>(initialValues);
+  const { values, touched, errors, setValues, setTouched, handleSubmit } =
+    useForm({ cards, bannedCountries, onSubmit });
 
-  const [errors, setErrors] = useState<Errors>({});
-
-  const [touched, setTouched] = useState<Touched>({
-    name: false,
-    cardNumber: false,
-    expirationDateMM: false,
-    expirationDateYY: false,
-    country: false,
-    cvv: false,
-  });
-
-  const setAllTouched = () => {
-    setTouched({
-      name: true,
-      cardNumber: true,
-      expirationDateMM: true,
-      expirationDateYY: true,
-      country: true,
-      cvv: true,
-    });
-  };
-
-  const handleValidaton = () => {
-    const newErrors = { ...errors };
-
-    const handleRequiredValidation = (name: string, error: string) => {
-      validateRequired(name, values, newErrors, touched, error);
-    };
-
-    handleRequiredValidation('name', 'Name is required');
-    handleRequiredValidation('country', 'Country is required');
-
-    const getCardNumberErrors = () => {
-      let cardErrors = '';
-      const isCardAlreadyAdded = (cardNumber: string) => {
-        return cards.some((card) => card.cardNumber === cardNumber);
-      };
-
-      cardErrors = touched?.cardNumber
-        ? validateLength(values.cardNumber, 16)
-        : '';
-
-      if (isCardAlreadyAdded(values.cardNumber)) {
-        cardErrors = 'Card already added';
-      } else {
-        cardErrors = cardErrors;
-      }
-
-      return cardErrors;
-    };
-
-    const getCountryErrors = () => {
-      let countryErrors = errors.country;
-
-      if (!values.country && touched.country) {
-        return 'Country is required';
-      }
-
-      const isCountryBanned = (country: string) => {
-        return bannedCountries.some(
-          (bannedCountry) => bannedCountry.value === country,
-        );
-      };
-
-      if (isCountryBanned(values.country)) {
-        countryErrors = 'Country is banned';
-      } else {
-        countryErrors = '';
-      }
-
-      return countryErrors;
-    };
-
-    const cvvErrors = touched?.cvv ? validateLength(values.cvv, 3) : '';
-
-    const expirationDateMMErrors = touched?.expirationDateMM
-      ? validationNumberRange(values.expirationDateMM, 1, 12)
-      : '';
-
-    const expirationDateYYErrors = touched?.expirationDateYY
-      ? // @todo don't hard code the year
-        validationNumberRange(values.expirationDateYY, 23, 99)
-      : '';
-
-    setErrors({
-      ...newErrors,
-      cardNumber: getCardNumberErrors(),
-      cvv: cvvErrors,
-      expirationDateMM: expirationDateMMErrors,
-      expirationDateYY: expirationDateYYErrors,
-      country: getCountryErrors(),
-    });
-  };
-
-  const isValid = useCallback(() => {
-    if (Object.values(touched).filter((t) => t === false).length > 0) {
-      return false;
-    }
-
-    return Object.values(errors).filter((error) => error !== '').length === 0;
-  }, [errors]);
-
-  useEffect(() => {
-    handleValidaton();
-  }, []);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setAllTouched();
-
-    if (isValid()) {
-      onSubmit(e);
-      setValues(initialValues);
-      setTouched({});
-      setErrors({});
-    }
-  };
-
-  // Listen for changes in the values state and validate the inputs
-  useEffect(() => {
-    handleValidaton();
-  }, [values, touched]);
   return (
     <form onSubmit={handleSubmit} className={styles['credit-card-form']}>
       <div className={styles['two-col']}>
